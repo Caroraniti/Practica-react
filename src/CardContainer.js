c8c6eeb3ab25ce3d9e0d3b78ec6ce790bd21fc56
unknown <Administrador@MDHYH011-02319.ad.buenosaires.gob.ar>
2022-01-27 13:55:36 -0300

    agrego estilo

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -5,11 +5,13 @@ const CardContainer = () => {
         <div>
             <h2>Titulo</h2>
             <a href="http://www.google.com">Soy un link</a>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
+            <div className="container-tarjetas">
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+            </div>
         </div>
     )
 }
